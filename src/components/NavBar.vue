2016893678088c86115df511cfd7663ca01f7032
clocky <jhj1390jhj@naver.com>
2021-01-15 00:20:38 +0900

    기능추가 : 앱바, 내비게이션 추가
    NavBar.vue component 생성

---
+++ src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav id="navbar">
+    <v-app-bar color="white" flat app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <router-link to="/" class="black--text" style="text-decoration: none">
+          CCAAFFEE
+        </router-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon size="25">
+          mdi-account-box
+        </v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list nav dense>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+  }),
+  methods: {},
+}
+</script>
+
+<style lang="scss"></style>
