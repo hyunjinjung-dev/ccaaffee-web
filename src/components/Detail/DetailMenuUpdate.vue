3e96cd6623950c22ce3dfb51bfa3a45101e10db7
clocky <jhj1390jhj@naver.com>
2021-02-25 15:02:11 +0900

    기능 추가 : DetailMenu 개선

--- src/components/Detail/DetailMenuUpdate.vue
+++ src/components/Detail/DetailMenuUpdate.vue
@@ -1,173 +1,170 @@
 <template>
-  <v-dialog
-    v-model="dialog"
-    persistent
-    transition="scroll-x-transition"
-    scrollable
-    max-width="500px"
+  <v-dialog v-model="dialog" persistent transition="scroll-x-transition" scrollable fullscreen>
+    <!-- max-width="500px"
     :fullscreen="breakPointXs ? true : false"
-    :hide-overlay="breakPointXs ? true : false"
-  >
+    :hide-overlay="breakPointXs ? true : false" -->
     <v-card class="pa-2">
       <v-card-title class="font-weight-bold subheading mb-5">{{ title }} 수정</v-card-title>
-      <v-card-text
-        :class="breakPointXs ? 'pa-0' : ''"
-        id="container"
-        ref="columnLeft"
-        style="max-height: 40%;"
-      >
-        <draggable v-model="formLeft" :options="{ group: 'menus' }" style="min-height: 10px;">
-          <template v-for="(item, index) in formLeft">
-            <v-container :key="index">
-              <v-sheet v-if="item.name == '구분선'">
-                <v-row align="center">
-                  <v-col cols="8" style="text-align:center;">
-                    <v-divider></v-divider>
-                  </v-col>
-                  <v-col cols="2">
-                    <v-btn icon @click="removeMenuConfirm(index, 'left')">
-                      <v-icon>mdi-minus-circle</v-icon>
-                    </v-btn>
-                  </v-col>
-                  <v-col cols="2">
-                    <v-btn icon @click="removeMenu(index, column)">
-                      <v-icon>mdi-unfold-more-horizontal</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-sheet>
-              <v-sheet v-else>
-                <v-row align="center">
-                  <v-col cols="6">
-                    <v-text-field
-                      v-model="item.name"
-                      :rules="menuNameRules"
-                      label="메뉴명"
-                      hide-details
-                      outlined
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="4">
-                    <v-text-field
-                      hide-details
-                      v-model="item.price"
-                      :rules="menuPriceRules"
-                      type="tel"
-                      label="가격"
-                      outlined
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="2" align="center">
-                    <v-btn icon @click="removeMenuConfirm(index, 'left')">
-                      <v-icon>mdi-minus-circle</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-                <v-row align="center">
-                  <v-col cols="10">
-                    <v-text-field
-                      hide-details
-                      v-model="formLeft[index].describe"
-                      :rules="menuDescribeRules"
-                      label="메뉴설명"
-                      outlined
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="2" align="center">
-                    <v-btn icon @click="removeMenu(index, column)">
-                      <v-icon>mdi-unfold-more-horizontal</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-sheet>
-            </v-container>
-          </template>
-        </draggable>
-        <div style="margin: 10px 0px; text-align: center;">
-          <v-btn class="mx-1" color="secondary" @click="addMenu('left')">메뉴 추가</v-btn>
-          <v-btn class="mx-1" color="secondary" @click="addDivider('left')">구분선 추가</v-btn>
-        </div>
+      <v-card-text :class="breakPointXs ? 'pa-0' : ''" ref="wrapper">
+        <v-row>
+          <v-col cols="12" md="6" sm="12" xs="12">
+            <draggable v-model="formLeft" :options="{ group: 'menus' }" style="min-height: 10px;">
+              <template v-for="(item, index) in formLeft">
+                <v-container :key="index">
+                  <v-sheet v-if="item.name == '구분선'">
+                    <v-row align="center">
+                      <v-col cols="8" style="text-align:center;">
+                        <v-divider></v-divider>
+                      </v-col>
+                      <v-col cols="2">
+                        <v-btn icon @click="removeMenuConfirm(index, 'left')">
+                          <v-icon>mdi-minus-circle</v-icon>
+                        </v-btn>
+                      </v-col>
+                      <v-col cols="2">
+                        <v-btn icon @click="removeMenu(index, column)">
+                          <v-icon>mdi-unfold-more-horizontal</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-sheet>
+                  <v-sheet v-else>
+                    <v-row align="center">
+                      <v-col cols="6">
+                        <v-text-field
+                          v-model="item.name"
+                          :rules="menuNameRules"
+                          label="메뉴명"
+                          hide-details
+                          outlined
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="4">
+                        <v-text-field
+                          hide-details
+                          v-model="item.price"
+                          :rules="menuPriceRules"
+                          type="tel"
+                          label="가격"
+                          outlined
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="2" align="center">
+                        <v-btn icon @click="removeMenuConfirm(index, 'left')">
+                          <v-icon>mdi-minus-circle</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                    <v-row align="center">
+                      <v-col cols="10">
+                        <v-text-field
+                          hide-details
+                          v-model="formLeft[index].describe"
+                          :rules="menuDescribeRules"
+                          label="메뉴설명"
+                          outlined
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="2" align="center">
+                        <v-btn icon @click="removeMenu(index, column)">
+                          <v-icon>mdi-unfold-more-horizontal</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-sheet>
+                </v-container>
+              </template>
+            </draggable>
+            <div style="margin: 10px 0px; text-align: center;">
+              <v-btn class="mx-1" color="secondary" @click="addMenu('left')">메뉴 추가</v-btn>
+              <v-btn class="mx-1" color="secondary" @click="addDivider('left')">구분선 추가</v-btn>
+            </div>
+          </v-col>
+
+          <v-col cols="12" md="6" sm="12" xs="12">
+            <draggable v-model="formRight" :options="{ group: 'menus' }" style="min-height: 10px;">
+              <template v-for="(item, index) in formRight">
+                <v-container :key="index">
+                  <v-sheet v-if="item.name == '구분선'">
+                    <v-row align="center">
+                      <v-col cols="8" style="text-align:center;">
+                        <v-divider></v-divider>
+                      </v-col>
+                      <v-col cols="2">
+                        <v-btn icon @click="removeMenuConfirm(index, 'right')">
+                          <v-icon>mdi-minus-circle</v-icon>
+                        </v-btn>
+                      </v-col>
+                      <v-col cols="2">
+                        <v-btn icon @click="removeMenu(index, column)">
+                          <v-icon>mdi-unfold-more-horizontal</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-sheet>
+                  <v-sheet v-else>
+                    <v-row align="center">
+                      <v-col cols="6">
+                        <v-text-field
+                          v-model="item.name"
+                          :rules="menuNameRules"
+                          label="메뉴명"
+                          hide-details
+                          outlined
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="4">
+                        <v-text-field
+                          hide-details
+                          v-model="item.price"
+                          :rules="menuPriceRules"
+                          type="tel"
+                          label="가격"
+                          outlined
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="2" align="center">
+                        <v-btn icon @click="removeMenuConfirm(index, 'right')">
+                          <v-icon>mdi-minus-circle</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                    <v-row align="center">
+                      <v-col cols="10">
+                        <v-text-field
+                          hide-details
+                          v-model="formRight[index].describe"
+                          :rules="menuDescribeRules"
+                          label="메뉴설명"
+                          outlined
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="2" align="center">
+                        <v-btn icon @click="removeMenu(index)">
+                          <v-icon>mdi-unfold-more-horizontal</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-sheet>
+                </v-container>
+              </template>
+            </draggable>
+
+            <div style="margin: 10px 0px; text-align: center;">
+              <v-btn class="mx-1" color="secondary" @click="addMenu('right')">메뉴 추가</v-btn>
+              <v-btn class="mx-1" color="secondary" @click="addDivider('right')">구분선 추가</v-btn>
+            </div>
+          </v-col>
+        </v-row>
       </v-card-text>
 
-      <v-divider class="mx-8 my-4"></v-divider>
+      <!-- <v-divider class="mx-8 my-4"></v-divider> -->
       <v-card-text
         :class="breakPointXs ? 'pa-0' : ''"
         id="container"
         ref="columnRight"
         style="max-height: 40%;"
       >
-        <draggable v-model="formRight" :options="{ group: 'menus' }" style="min-height: 10px;">
-          <template v-for="(item, index) in formRight">
-            <v-container :key="index">
-              <v-sheet v-if="item.name == '구분선'">
-                <v-row align="center">
-                  <v-col cols="8" style="text-align:center;">
-                    <v-divider></v-divider>
-                  </v-col>
-                  <v-col cols="2">
-                    <v-btn icon @click="removeMenuConfirm(index, 'right')">
-                      <v-icon>mdi-minus-circle</v-icon>
-                    </v-btn>
-                  </v-col>
-                  <v-col cols="2">
-                    <v-btn icon @click="removeMenu(index, column)">
-                      <v-icon>mdi-unfold-more-horizontal</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-sheet>
-              <v-sheet v-else>
-                <v-row align="center">
-                  <v-col cols="6">
-                    <v-text-field
-                      v-model="item.name"
-                      :rules="menuNameRules"
-                      label="메뉴명"
-                      hide-details
-                      outlined
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="4">
-                    <v-text-field
-                      hide-details
-                      v-model="item.price"
-                      :rules="menuPriceRules"
-                      type="tel"
-                      label="가격"
-                      outlined
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="2" align="center">
-                    <v-btn icon @click="removeMenuConfirm(index, 'right')">
-                      <v-icon>mdi-minus-circle</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-                <v-row align="center">
-                  <v-col cols="10">
-                    <v-text-field
-                      hide-details
-                      v-model="formRight[index].describe"
-                      :rules="menuDescribeRules"
-                      label="메뉴설명"
-                      outlined
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="2" align="center">
-                    <v-btn icon @click="removeMenu(index)">
-                      <v-icon>mdi-unfold-more-horizontal</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-sheet>
-            </v-container>
-          </template>
-        </draggable>
-
-        <div style="margin: 10px 0px; text-align: center;">
-          <v-btn class="mx-1" color="secondary" @click="addMenu('right')">메뉴 추가</v-btn>
-          <v-btn class="mx-1" color="secondary" @click="addDivider('right')">구분선 추가</v-btn>
-        </div>
       </v-card-text>
 
       <v-card-actions class="mt-5">
@@ -234,17 +231,15 @@ export default {
       this.$emit("closeBtnClicked")
     },
     fetch() {
-      if (this.store.menusLeft.length > 0) {
-        // this.form = this.store.menus
+      if (this.store.menusLeft && this.store.menusLeft.length > 0) {
         this.formLeft = JSON.parse(JSON.stringify(this.store.menusLeft))
       } else {
         this.formLeft = [{ name: "", price: "", describe: "" }]
       }
-      if (this.store.menusRight.length > 0) {
-        // this.form = this.store.menus
+      if (this.store.menusRight && this.store.menusRight.length > 0) {
         this.formRight = JSON.parse(JSON.stringify(this.store.menusRight))
       } else {
-        // this.formRight = [{ name: "", price: "", describe: "" }]
+        this.formRight = [{ name: "", price: "", describe: "" }]
       }
     },
     async updateBtnClicked() {
@@ -289,17 +284,15 @@ export default {
       this.removeConfirmDialog = false
     },
     async addMenu(column) {
+      if (column == "left") {
+        this.formLeft.push({ name: "", price: "", describe: "" })
+      } else {
+        this.formRight.push({ name: "", price: "", describe: "" })
+      }
       // 메뉴 추가와 스크롤 다운이 동기로 진행되기 때문에 메뉴 추가를 비동기 처리하여
       // 처리 후의 scrollHeight를 잡아준다.
-      if (column == "left") {
-        await this.formLeft.push({ name: "", price: "", describe: "" })
-        var columnLeft = this.$refs.columnLeft
-        columnLeft.scrollTop = columnLeft.scrollHeight
-      } else {
-        await this.formRight.push({ name: "", price: "", describe: "" })
-        var columnRight = this.$refs.columnRight
-        columnRight.scrollTop = columnRight.scrollHeight
-      }
+      // var wrapper = this.$refs.wrapper
+      // wrapper.scrollTop = wrapper.scrollHeight
     },
     async addDivider(column) {
       if (column == "left") {
